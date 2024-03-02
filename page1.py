c83fd0aae86443d79a563953b5fd057e5135695d
harni10 <harni.501@sma.belajar.id>
2024-03-02 13:28:52 +0900

    hiug

--- page1.py
+++ page1.py
@@ -5,3 +5,4 @@ def page_1():
     st.write('Halaman ini digunakan untuk Intro')
     st.write('halaman ini menampilkan kartun')
     st.write('Kartun ini menceritakan tentang persahabatan seorang anak perempuan bernama Masha dengan seekor teman beruangnya, Misha. Ciri khas Masha adalah selalu menggunakan baju berwarna ungu dan tudung kepala. Masha adalah anak yang ceria dan memiliki rasa penasaran yang tinggi.')
+    st.image("marsha.jpg",width=100)
