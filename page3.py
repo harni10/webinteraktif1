72b6be6b98a1d004d0857d8f773ab883090ef6f4
harni10 <harni.501@sma.belajar.id>
2024-03-02 12:30:32 +0900

    nbsdh

--- page3.py
+++ page3.py
@@ -2,4 +2,9 @@ import streamlit as st
 
 def page_3():
     st.title("Halaman 3")
-    st.write('Halaman ini digunakan untuk Menampilkan Rumus Matematika')
+    st.write('Menampilkan Halaman dari file MD (MarkDown)')
+
+    with open('nama.md', 'r') as file:
+        isi_teks = file.read()
+    st.markdown(isi_teks)
+        
