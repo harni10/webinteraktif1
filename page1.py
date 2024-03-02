7c0eee483d4a1da36489b766c8ef87a213ec2c13
harni10 <harni.501@sma.belajar.id>
2024-03-02 12:53:01 +0900

    bhsa

--- page1.py
+++ page1.py
@@ -3,3 +3,5 @@ import streamlit as st
 def page_1():
     st.title("Halaman 1")
     st.write('Halaman ini digunakan untuk Intro')
+    st.write('halaman ini menampilkan kartun')
+    st.write('Kartun ini menceritakan tentang persahabatan seorang anak perempuan bernama Masha dengan seekor teman beruangnya, Misha. Ciri khas Masha adalah selalu menggunakan baju berwarna ungu dan tudung kepala. Masha adalah anak yang ceria dan memiliki rasa penasaran yang tinggi.7')
