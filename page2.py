1f6873e716e3d9fb9512197967ab0fd4d8581416
harni10 <harni.501@sma.belajar.id>
2024-03-02 13:01:12 +0900

    vhiu

--- page2.py
+++ page2.py
@@ -3,5 +3,4 @@ import streamlit as st
 def page_2():
     st.title("Halaman 2")
     st.write('Halaman ini digunakan untuk  Menampilkan Youtube video')
-    st.write('ayo kita nonton bersama harni')
     st.video('https://www.youtube.com/watch?v=uPTjYnBpwLY')
